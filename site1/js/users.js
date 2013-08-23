df9eec4bc10398a603d8211ce1542c0e5113b796
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-08-23 14:31:08 -0700

    initial commit for users.js

---
+++ site1/js/users.js
@@ -0,0 +1,38 @@
+$(function(){
+    var users = [
+        {'status': 'online', 'name': 'Alex Penny'},
+        {'status': 'away', 'name': 'Alan Grinshtein'},
+        {'status': 'busy', 'name': 'Glenn Danzig'},
+        {'status': 'offline', 'name': 'Jello Biafra'},
+        {'status': 'online', 'name': 'Alex Penny'},
+        {'status': 'away', 'name': 'Alan Grinshtein'},
+        {'status': 'busy', 'name': 'Glenn Danzig'},
+        {'status': 'offline', 'name': 'Jello Biafra'},
+        {'status': 'online', 'name': 'Alex Penny'},
+        {'status': 'away', 'name': 'Alan Grinshtein'},
+        {'status': 'busy', 'name': 'Glenn Danzig'},
+        {'status': 'offline', 'name': 'Jello Biafra'},
+        {'status': 'online', 'name': 'Alex Penny'},
+        {'status': 'away', 'name': 'Alan Grinshtein'},
+        {'status': 'busy', 'name': 'Glenn Danzig'},
+        {'status': 'offline', 'name': 'Jello Biafra'},
+        {'status': 'online', 'name': 'Alex Penny'},
+        {'status': 'away', 'name': 'Alan Grinshtein'},
+        {'status': 'busy', 'name': 'Glenn Danzig'},
+        {'status': 'offline', 'name': 'Jello Biafra'},
+    ]
+
+    var contacts_list = $('<ul>')
+    var contact_count = 0
+
+    $.each(users, function(key, value){
+        var status_dot, name;
+        var contact = $('<li>')
+        name = value['name'];
+        status_dot = $("<span>").addClass('icon-circle icon-small ' + value['status'])
+        if(value['status'] == 'online') contact_count++;
+        contact.append(status_dot).append(name)
+        contacts_list.append(contact)
+    })
+    $('.contacts').append(contacts_list)
+})
